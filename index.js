5ad2338361569f0e70b46884092b4b50dca45846
mrvxo <mrvxo@protonmail.com>
2020-10-27 11:03:37 +0100

    v1.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const socket = require('os');
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const config  = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -22,22 +23,24 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-	client.user.setActivity("critiquer Bilal!");
-});
+	client.user.setActivity("Je suis TuxBot, le bot qui vit de l'OpenSource ! ;)");
+    const channel = client.channels.cache.find(channel => channel.name === "annonces-du-bot-pronote")
+    channel.send({embed: {title: "Démarrage de Tuxbot",description: "Instance lancé sur " + socket.hostname()}})
 
+    });
 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 	//Pour être sur que le MDP disparaisse si ce n'est pas dans les MPS du bot
 	if(message.channel.type == Discord.GuildChannel) message.channel.bulkDelete(1);
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 	if (!client.commands.has(command)) return message.reply("Commande innexistante");
 	try {
-		client.commands.get(command).execute(message, args, Discord, client);
+		client.commands.get(command).execute(message, args, Discord, client, config);
 	}catch (error) {
 		console.error(error);
 		message.reply('Une erreur a surgit dans le code... \n' + error);
 	}
 });
-client.login(token).then(console.log("Pronote bot parait"));
+client.login(config.token).then(console.log("Pronote bot parait"));
