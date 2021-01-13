53f8fa61c1cf4d3f601bf022f6791da56838908d
mrvxo <mrvxo@protonmail.com>
2021-01-13 11:08:37 +0000

    Annulation de Heroku

--- index.js
+++ index.js
@@ -23,9 +23,34 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
 	client.user.setActivity("Je suis TuxBot, le bot qui vit avec FSF! ;)");
-    //const channel = client.channels.cache.find(channel => channel.name === "log")
-    //channel.bulkDelete(80);
-    //channel.send({embed: {title: "Démarrage de Tuxbot",description: "Instance lancé sur " + socket.hostname()+ " :smile:"}})
+  const channel = client.channels.cache.find(channel => channel.name === "pwned-devoirs")
+  channel.bulkDelete(80);
+	setInterval(async () => { 
+		for(const [key, value] of client.listeSession){
+			const marks = await session.homeworks();
+				exampleEmbed.setTitle('Devoirs');
+				var devoirJour = "";
+				var Jour = marks[0].for;
+				for(var i = 0; i < marks.length; i++){
+					//Classifiez dans la même journée
+					if(Jour.getDay() == marks[i].for.getDay()){
+						devoirJour += ` **En: ${marks[i].subject}** :\n  __${marks[i].description}__ \n **Leçon:** ${("Aucune leçon" || 
+						marks[i].lesson)}`;
+						if(marks[i].files.length >= 1){
+							for(let [name, url] of Object.entries(marks[i].files))devoirJour += `**Fichiers:**[${"Fichier"}](${url.url})`;
+						}
+						devoirJour += "\n\n"
+					//Sinon on recommence un tableau
+					}else{
+						Jour = marks[i].for;
+						exampleEmbed.addField(`Devoir pour le ${client.jourSemaine[marks[i].for.getDay() - 1]} ${marks[i].for.getDate()} / ` +
+						`${marks[i].for.getMonth() + 1} `,
+						` => ${devoirJour}`);
+						devoirJour = "";
+					}
+				}
+		}
+	}, 60000*60);
     
 });
 
